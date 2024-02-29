ef8cce244c27aa82f46e80060ec44c4a361865b9
nicoletarusnac <nr1227e@gre.ac.uk>
2024-02-29 10:51:32 +0000

    Add: Iventory and Journal classes

--- libarysystem_WpfApp/libarysystem_WpfApp/JournalLookUp.xaml.cs
+++ libarysystem_WpfApp/libarysystem_WpfApp/JournalLookUp.xaml.cs
@@ -23,5 +23,10 @@ namespace libarysystem_WpfApp
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
